ee6767e5106aaec1b9985a383e0f486195848f6b
Abdel00zz <49434234+Abdel00zz@users.noreply.github.com>
2025-01-26 11:41:14 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+from flask import Flask
 from app import create_app
 
 app = create_app()
+
+@app.route('/')
+def health():
+    return 'OK', 200
 
 if __name__ == '__main__':
     app.run(debug=True)
